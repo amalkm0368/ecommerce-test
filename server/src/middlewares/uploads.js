dd70058c48c55bd5ae9a87115297f8d48e534375
amal <amalkm0368@gmail.com>
2025-12-16 21:04:50 +0530

    first push2

--- server/src/middlewares/uploads.js
+++ server/src/middlewares/uploads.js
@@ -1,6 +1,7 @@
 import multer from "multer"
-import { CloudinaryStorage } from "multer-storage-cloudinary"
+import { CloudinaryStorage } from "@fluidjs/multer-cloudinary"
 import cloudinary from "../config/cloudinary.js"
+
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
@@ -8,5 +9,6 @@ const storage = new CloudinaryStorage({
     allowed_formats: ["jpg", "jpeg", "png"],
   },
 })
+
 const upload = multer({ storage })
 export default upload
